a7406df447c3c1ffc27f8521e3046a9e6417587e
GitHub Action <action@github.com>
2023-02-17 22:07:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/domonion/kgeorgiymediaplayer/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/domonion/kgeorgiymediaplayer/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.domonion.kgeorgiymediaplayer.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.domonion.kgeorgiymediaplayer.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/domonion/kgeorgiymediaplayer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/domonion/kgeorgiymediaplayer/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.domonion.kgeorgiymediaplayer.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.domonion.kgeorgiymediaplayer.MyBundle
 
 class MyProjectService(project: Project) {
 
